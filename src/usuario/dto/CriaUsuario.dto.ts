e62e448b25ad6d3e2f02084025c916b63ea8cc32
HenriqueTogo <henrique.togo@gmail.com>
2025-04-04 17:25:22 -0300

    entidade do usuário

--- src/usuario/dto/CriaUsuario.dto.ts
+++ src/usuario/dto/CriaUsuario.dto.ts
@@ -1,4 +1,5 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+import { EmailEhUnico } from "../validacao/email-eh-unico.validator";
 
 export class CriaUsuarioDTO {
 
@@ -6,6 +7,7 @@ export class CriaUsuarioDTO {
     nome: string;
 
     @IsEmail(undefined, { message: 'O e-mail inválido' })
+    @EmailEhUnico({ message: 'Já existe um usuário com esse e-mail' })
     email: string;
 
     @MinLength(6, { message: 'A senha deve ter no mínimo 6 caracteres' })
--- src/usuario/usuario.repository.ts
+++ src/usuario/usuario.repository.ts
@@ -1,11 +1,12 @@
 import { Injectable } from "@nestjs/common";
+import { UsuarioEntity } from "./usuario.entity";
 
 @Injectable()
 export class UsuarioRepository {
     
-    private usuarios : string[] = [];
+    private usuarios : UsuarioEntity[] = [];
     
-    async salvar(usuario) {
+    async salvar(usuario : UsuarioEntity) {
         this.usuarios.push(usuario);        
     }
 
@@ -13,4 +14,10 @@ export class UsuarioRepository {
        return this.usuarios;
     }
 
+    async existeComEmail(email: string) {
+        const possivelUsuario = this.usuarios.find(
+            usuario => usuario.email === email
+        );
+        return possivelUsuario !== undefined;
+    }
 }
